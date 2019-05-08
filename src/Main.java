228e03d5c870bbbfa6e312a6c27271f6a0d28689
Leon <stbirb@yandex.ru>
2019-05-08 17:11:43 +0300

    add remove of songs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
+
+//        Create a program that implements a playlist for songs
+//        Create a Song class having Title and Duration for a song/
+//        The program will have an Album class containing a list of songs.
+//        The albums will be stored in an ArrayList
+//        Songs from different albums can be added to the playlist and will appear in the list in the order
+//        they are added.
+//        Once the songs have been added to the playlist, create a menu of options to:
+//        Quit, Skip forward to the next song, skip backwards to a previous song. Replay the current song.
+//        List the songs in the playlist
+//        A song must exist in an album before it can be added to the playlist (so you can only play songs that
+//        you own).
+//        Hint To replay a song, consider what happened when we went back and forth from a city before we
+//        started tracking the direction we were going.
+//        As an optional extra, provide an option to remove the current song from the playlist
+//        (hint: listiterator.remove()
+
 
 public class Main {
 
@@ -36,36 +50,70 @@ album = new Album("Soul");
 
 play(playList);
 //        System.out.println(playList.get(3).getTitle());
-
-
-
-
-
-
     }
     public static void play(LinkedList<Song> playList){
 
-
-
-        Iterator<Song> i = playList.iterator();
-        System.out.println(i.next().getTitle());
-
+        ListIterator<Song> i = playList.listIterator();
 
         Scanner scanner = new Scanner(System.in);
 
         boolean flag = true;
+        boolean forward = true;
+        int choice = 9;
+
         while(flag){
 
-        int choise = scanner.nextInt();
-        scanner.nextLine();
-
-        switch(choise){
-            case 1: System.out.println(i.next().getTitle());
+        switch(choice){
+            case 1: if (!forward && i.hasNext()){i.next();}
+                if (!i.hasNext()) {
+                    System.out.println("playList was end");
+                    forward = false;
+                } else {
+                    System.out.println(i.next().getTitle());
+                    forward = true;
+                }
             break;
-            case 2: flag = false;
+            case 2: if  (forward && i.hasPrevious()){i.previous();}
+                if (!i.hasPrevious()) {
+                    System.out.println("playList was end");
+                    forward = true;
+                } else {System.out.println(i.previous().getTitle());
+                    forward = false;
+                }
             break;
+            case 3: if (forward) {System.out.println(i.previous().getTitle());
+            forward = false;}
+            else {System.out.println(i.next().getTitle());
+            forward = true;}
+            break;
+            case 4:
+                Iterator<Song> j = playList.iterator();
+                while(j.hasNext()){
+                    System.out.println(j.next().getTitle());
+                }
+            break;
+            case 5: if (forward && i.hasPrevious()) {System.out.println(i.previous().getTitle() + " was delete");
+            i.remove();
+            }
+            else if (!forward && i.hasNext()){System.out.println(i.next().getTitle() + " was delete");
+            i.remove();
+            }
+                break;
+            case 9:
+                System.out.println(
+                        "1 - next song\n" +
+                        "2 - previous song\n" +
+                        "3 - replay song\n" +
+                        "4 - print list of songs\n" +
+                        "5 - delete current song\n" +
+                        "9 - show menu\n" +
+                        "0 - exit");
+                break;
+            case 0: flag = false;
+                break;
         }
+            choice = scanner.nextInt();
+            scanner.nextLine();
 }
-
     }
 }
